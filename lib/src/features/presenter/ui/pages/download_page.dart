b90c18e824b8771fb6d54d94f97107893b4d0d3b
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-10 20:22:52 -0300

    add redirection in Transference download

--- lib/src/features/presenter/ui/pages/download_page.dart
+++ lib/src/features/presenter/ui/pages/download_page.dart
@@ -1,7 +1,9 @@
 import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_downloads/src/features/presenter/ui/moleculs/container_manga_download.dart';
 import 'package:manga_easy_downloads/src/features/presenter/ui/organisms/list_manga_download.dart';
 import 'package:manga_easy_themes/manga_easy_themes.dart';
+import 'package:reorderables/reorderables.dart';
 
 class DownloadPage extends StatefulWidget {
   static const route = '/downloads';
@@ -10,6 +12,12 @@ class DownloadPage extends StatefulWidget {
   @override
   State<DownloadPage> createState() => _DownloadPageState();
 }
+
+List<Widget> _items = [
+  const ContainerMangaDownload(),
+  const ContainerMangaDownload(),
+  const ContainerMangaDownload(),
+];
 
 class _DownloadPageState extends State<DownloadPage> {
   bool isSearch = false;
@@ -71,11 +79,11 @@ class _DownloadPageState extends State<DownloadPage> {
                 size: 30,
               ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: CustomScrollView(
           slivers: [
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: Row(
                 children: [
                   Column(
@@ -88,11 +96,33 @@ class _DownloadPageState extends State<DownloadPage> {
                 ],
               ),
             ),
+            const SliverVisibility(
+              sliver: SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                sliver: SliverToBoxAdapter(
+                  child: CoffeeText(
+                    text: 'Em Transferência',
+                    typography: CoffeeTypography.title,
+                  ),
+                ),
+              ),
+            ),
             SliverVisibility(
               visible: true,
-              sliver: ListMangaDownload(title: 'Em transferência'),
+              sliver: ReorderableSliverList(
+                delegate: ReorderableSliverChildListDelegate(_items),
+                onReorder: (oldIndex, newIndex) {
+                  setState(() {
+                    if (newIndex > oldIndex) {
+                      newIndex -= 1;
+                    }
+                    final item = _items.removeAt(oldIndex);
+                    _items.insert(newIndex, item);
+                  });
+                },
+              ),
             ),
-            ListMangaDownload(title: 'Baixados'),
+            const ListMangaDownload(title: 'Baixados'),
           ],
         ),
       ),
