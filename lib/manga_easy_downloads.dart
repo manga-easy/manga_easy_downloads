4fec470eea19411f6fda4c35ee6d8e3ae37d611a
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-14 14:20:53 -0300

    fix

--- lib/manga_easy_downloads.dart
+++ lib/manga_easy_downloads.dart
@@ -1,1 +1,3 @@
 library manga_easy_downloads;
+
+export 'src/features/presenter/ui/pages/download_page.dart';
--- lib/src/features/data/mappers/download_mapper.dart
+++ lib/src/features/data/mappers/download_mapper.dart
@@ -4,21 +4,22 @@ import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 class DownloadMapper {
   DownloadEntity fromJson(Map<String, dynamic> json) {
     return DownloadEntity(
-        id: json['id'] ?? '',
-        idHost: json['idHost'],
-        uniqueid: Helps.convertUniqueid(json['uniqueid'] ?? json['idManga']),
-        detalhesManga: DetalhesManga.fromJson(Map.from(json['detalhesManga'])),
-        idUser: json['idUser'],
-        erro: json['erro'] != null
-            ? json['erro'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-            : [],
-        abaixar: json['abaixar'] != null
-            ? json['abaixar'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-            : [],
-        baixado: json['baixado'] != null
-            ? json['baixado'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-            : [],
-        dataCria: json['dataCria']);
+      id: json['id'] ?? '',
+      idHost: json['idHost'],
+      uniqueid: Helps.convertUniqueid(json['uniqueid'] ?? json['idManga']),
+      detalhesManga: DetalhesManga.fromJson(Map.from(json['detalhesManga'])),
+      idUser: json['idUser'],
+      erro: json['erro'] != null
+          ? json['erro'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
+          : [],
+      abaixar: json['abaixar'] != null
+          ? json['abaixar'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
+          : [],
+      baixado: json['baixado'] != null
+          ? json['baixado'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
+          : [],
+      dataCria: json['dataCria'],
+    );
   }
 
   Map<String, dynamic> toJson(DownloadEntity data) {
