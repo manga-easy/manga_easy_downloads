180b0b9079731cb23b62f38347d0796286df234f
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-17 15:20:43 -0300

    update Mapper

--- lib/src/features/data/mappers/download_mapper.dart
+++ lib/src/features/data/mappers/download_mapper.dart
@@ -5,34 +5,26 @@ class DownloadMapper {
   DownloadEntity fromJson(Map<String, dynamic> json) {
     return DownloadEntity(
       id: json['id'] ?? '',
-      idHost: json['idHost'],
-      uniqueid: Helps.convertUniqueid(json['uniqueid'] ?? json['idManga']),
-      detalhesManga: DetalhesManga.fromJson(Map.from(json['detalhesManga'])),
+      uniqueid: json['uniqueid'],
       idUser: json['idUser'],
-      erro: json['erro'] != null
-          ? json['erro'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-          : [],
-      abaixar: json['abaixar'] != null
-          ? json['abaixar'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-          : [],
-      baixado: json['baixado'] != null
-          ? json['baixado'].map<Chapter>((e) => Chapter.fromJson(e)).toList()
-          : [],
-      dataCria: json['dataCria'],
+      createAt: json['createAt'],
+      folder: json['folder'],
+      manga: json['manga'],
+      status: json['status'],
+      chapters: json['chapters'],
     );
   }
 
   Map<String, dynamic> toJson(DownloadEntity data) {
     return {
       'id': data.id,
-      'idHost': data.idHost,
       'uniqueid': data.uniqueid,
       'idUser': data.idUser,
-      'abaixar': data.abaixar.map((v) => v.toJson()).toList(),
-      'erro': data.erro.map((v) => v.toJson()).toList(),
-      'baixado': data.baixado.map((v) => v.toJson()).toList(),
-      'dataCria': data.dataCria,
-      'detalhesManga': data.detalhesManga.toJson(),
+      'createAt': data.createAt,
+      'folder': data.folder,
+      'manga': data.manga,
+      'status': data.status,
+      'chapters': data.chapters,
     };
   }
 }
