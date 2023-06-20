bf556ffc2b8ecc1ba41b94812f5ffee43e71d32d
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-20 16:29:26 -0300

    add function DownloadFile

--- lib/src/features/presenter/controllers/download_controller.dart
+++ lib/src/features/presenter/controllers/download_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:manga_easy_downloads/src/features/domain/entities/download_entity.dart';
 import 'package:manga_easy_downloads/src/features/domain/services/service_download.dart';
 import 'package:manga_easy_downloads/src/features/domain/usecases/create_usecase.dart';
@@ -9,10 +10,8 @@ import 'package:manga_easy_downloads/src/features/domain/usecases/delete_usecase
 import 'package:manga_easy_downloads/src/features/domain/usecases/get_usecase.dart';
 import 'package:manga_easy_downloads/src/features/domain/usecases/list_usecase.dart';
 import 'package:manga_easy_downloads/src/features/domain/usecases/update_usecase.dart';
-import 'package:manga_easy_sdk/manga_easy_sdk.dart';
-import 'package:path_provider/path_provider.dart';
 
-class DownloadController {
+class DownloadController extends ChangeNotifier {
   final CreateUsecase createCase;
   final UpdateUsecase updateCase;
   final DeleteUsecase deleteCase;
@@ -31,75 +30,16 @@ class DownloadController {
     this.service,
   );
 
+  void init() {
+    listDownload();
+    notifyListeners();
+  }
 
-  var downloadMangaMetal = DownloadEntity(
-    uniqueid: "MentallyBroken",
-    idUser: 1,
-    createAt: DateTime.now(),
-    manga: Manga(
-      capa: "http://api.lucas-cm.com.br/mentally-broken/capa.png",
-      href: "easy-scanMentallyBroken",
-      title: "Mentally Broken",
-      idHost: 7,
-      uniqueid: "MentallyBroken",
-    ),
-    folder: '/storage/emulated/0/Download/',
-    chapters: [
-      ChapterStatus(
-        Chapter(
-          title: '1',
-          href: "easy-scanMentallyBroken1",
-          id: "MentallyBroken1",
-          imagens: [
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/1.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/2.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/3.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/4.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/5.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/6.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/7.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-            ImageChapter(
-              src: "http://api.lucas-cm.com.br/mentally-broken/01/8.jpg",
-              state: 1,
-              tipo: TypeFonte.image,
-            ),
-          ],
-          number: 1,
-          date: "2023-06-16 16:49:58.507446",
-        ),
-        Status.todo,
-      ),
-    ],
-    status: Status.todo,
-  );
+  List<DownloadEntity> listMangaDownload = [];
+  void listDownload() async {
+    listMangaDownload = await listCase.list();
+    notifyListeners();
+  }
 
   var folder = '/storage/emulated/0/Downloads';
 
@@ -121,7 +61,36 @@ class DownloadController {
     return '/storage/emulated/0/Downloads';
   }
 
-  Future<void> downloadFile() async {
-    await service.downloadFile(downloadMangaMetal);
+  var listTodo = [];
+
+  void downloadFile() async {
+    try {
+      listTodo =
+          listMangaDownload.where((e) => e.status == Status.todo).toList();
+      for (var todo in listTodo) {
+        await service.downloadFile(todo);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String> calculateFolderSize(String folderPath) async {
+    try {
+      final folder = Directory(folderPath);
+      int totalSize = 0;
+
+      if (await folder.exists()) {
+        await for (var entity in folder.list(recursive: true)) {
+          if (entity is File) {
+            totalSize += await entity.length();
+          }
+        }
+      }
+      notifyListeners();
+      return '$totalSize';
+    } catch (e) {
+     return 'N/A';
+    }
   }
 }
